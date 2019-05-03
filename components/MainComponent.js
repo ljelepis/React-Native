99cede41b736f52804e7b7a73dce4d7e91dd91a5
Lindsey Jelepis <lin.jelepis@gmail.com>
2019-05-03 13:29:01 -0700

    Navigation Part 2

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
+import Home from './HomeComponent';
 import Menu from './MenuComponent';
-import { DISHES } from '../shared/dishes';
 import DishDetail from './DishdetailComponent';
 import { View, Platform } from 'react-native';
-import { createStackNavigator } from 'react-navigation';
+import { createStackNavigator, createDrawerNavigator } from 'react-navigation';
 
 const MenuNavigator = createStackNavigator({//created a new component MenuNavigator component, which is a stackNavigator component.
   Menu: { screen: Menu },//this is a JS object here, with the parameter screen, which specifies the component for which you navigate when you make this choice here.
@@ -19,22 +19,53 @@ const MenuNavigator = createStackNavigator({//created a new component MenuNaviga
         color: '#fff'
       }
     }
-  }
-);//set up first Navigator.
+  });//set up first Navigator.
 //this one will use the menu component and the Dishdetail component and set up navigation between these two components.
+
+const HomeNavigator = createStackNavigator({
+  Home: { screen: Home }//this is a JS object here, with the parameter screen, which specifies the component for which you navigate when you make this choice here.
+}, {
+    navigationOptions: {
+      headerStyle: {//where we specify the color for the header for our stackNavigator.
+        backgroundColor: '#512DA8'
+      },
+      headerTintColor: '#fff',
+      headerTitleStyle: {
+        color: '#fff'
+      }
+    }
+});//set up second Navigator.
+
+const MainNavigator = createDrawerNavigator({
+  Home: {
+    screen: HomeNavigator,
+    navigationOptions: {
+      title: 'Home',
+      drawerLabel: 'Home'
+    }
+  },
+  Menu: {
+    screen: MenuNavigator,
+    navigationOptions: {
+      title: 'Menu',
+      drawerLabel: 'Menu'
+    }
+  }
+}, {
+  drawerBackgrounColor: '#D1C4E9'});
 
 class Main extends Component {//created the main class component. Inside defined the constructor with the props.
 
   render() {
- //platform that we imported here, gives me access to specific platform on which React App is running.
+    //platform that we imported here, gives me access to specific platform on which React App is running.
     return (//two items Menu and Dishdetail being returned here, and that can't be done, so I hae to inclose that in a view
-        <View style={{flex: 1, paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight }}>
-        <MenuNavigator />
-        </View>//it was an array, but had to return that from single element, so added [0]
-        //onPress={(dishId) => this.onDishSelect(dishId)} is how I'm passing in handling the user interactions through this parameter, 
-        //that will result in a call to this function here, the dishId, this.onDishselect(dishId).
+      <View style={{ flex: 1, paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight }}>
+        <MainNavigator />
+      </View>//it was an array, but had to return that from single element, so added [0]
+      //onPress={(dishId) => this.onDishSelect(dishId)} is how I'm passing in handling the user interactions through this parameter, 
+      //that will result in a call to this function here, the dishId, this.onDishselect(dishId).
     );//removed <view style={{flex: 1}} because it was causing the view to be stretched.
   }
 }
-  
+
 export default Main;
