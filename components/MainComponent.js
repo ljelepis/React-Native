23391804f38a747a835c0ee994169d5a62edb1be
Lindsey Jelepis <lin.jelepis@gmail.com>
2019-05-13 15:14:06 -0700

    Favorites

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -5,6 +5,7 @@ import About from './AboutComponent';
 import Contact from './ContactComponent';
 import DishDetail from './DishdetailComponent';
 import Reservation from './ReservationComponent';
+import Favorites from './FavoriteComponent';
 import { View, Platform, Text, ScrollView, Image, StyleSheet } from 'react-native';
 import { createStackNavigator, createDrawerNavigator, DrawerItems, SafeAreaView } from 'react-navigation';
 import { Icon } from 'react-native-elements';
@@ -120,6 +121,23 @@ const ReservationNavigator = createStackNavigator({
    })
 })
 
+const FavoritesNavigator = createStackNavigator({
+  Favorites: { screen: Favorites }
+}, {
+  navigationOptions: ({ navigation }) => ({
+    headerStyle: {
+        backgroundColor: "#512DA8"
+    },
+    headerTitleStyle: {
+        color: "#fff"            
+    },
+    headerTintColor: "#fff",
+    headerLeft: <Icon name="menu" size={24}
+    color='white'
+    onPress={ () => navigation.toggleDrawer() } />   
+  })
+})
+
 const CustomDrawerContentComponent = (props) => (
   <ScrollView>
     <SafeAreaView style={styles.container}
@@ -182,6 +200,21 @@ const MainNavigator = createDrawerNavigator({
           color={tintColor}
           />
       )
+    }
+  },
+  Favorites:
+  { screen: FavoritesNavigator,
+    navigationOptions: {
+      title: 'My Favorites',
+      drawerLabel: 'My Favorites',
+      drawerIcon: ({ tintColor, focused }) => (
+        <Icon
+          name='heart'
+          type='font-awesome'            
+          size={24}
+          color={tintColor}
+        />
+      ),
     }
   },
   Reservation:
