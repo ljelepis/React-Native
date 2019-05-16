698edbd0e2ec5f55be9272c6077dfde8271f0e7a
Lindsey Jelepis <lin.jelepis@gmail.com>
2019-05-16 15:03:22 -0700

    Animations

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, ScrollView, View } from 'react-native';
+import { Text, View, Animated, Easing } from 'react-native';
 import { Card } from 'react-native-elements';
 import { baseUrl } from '../shared/baseUrl';
 import { connect } from 'react-redux';
@@ -50,32 +50,70 @@ function RenderItem(props) {
 
 class Home extends Component {//removed state in Home Component because we no longer need it there.
 
+    constructor(props) {
+        super(props);
+        this.animatedValue = new Animated.Value(0);
+    }
 
     static navigationOptions = {
         title: 'Home'
     };
 
-    render() {
-        return(
-            <ScrollView>
-                <RenderItem
-                    item={this.props.dishes.dishes.filter((dish) => dish.featured)[0]}
-                    isLoading={this.props.dishes.isLoading}
-                    errMess={this.props.dishes.errMess}
-                    />
-                <RenderItem
-                    item={this.props.promotions.promotions.filter((promo) => promo.featured)[0]}
-                    isLoading={this.props.promotions.isLoading}
-                    errMess={this.props.promotions.errMess}
-                    />
-                <RenderItem
-                    item={this.props.leaders.leaders.filter((leader) => leader.featured)[0]}
-                    isLoading={this.props.leaders.isLoading}
-                    errMess={this.props.leaders.errMess}
-                    />                    
-            </ScrollView>
-        );
+    componentDidMount() {
+        this.animate();
     }
+
+    animate() {
+        this.animatedValue.setValue(0);//we can use this value to control certain properties.
+        Animated.timing(//animated timing takes a value, and then you can specify how you want to change that value as a function.
+            this.animatedValue,
+        {
+            toValue: 8,
+            duration: 8000,
+            easing: Easing.linear
+        }
+        ).start(() => this.animate());//we are looping through these changes. That's what's specifying here.
+    }
+
+    render() {//interpolation, meaning, as the value changes, map that value to a corresponding different value here.
+        const xpos1 = this.animatedValue.interpolate({
+            inputRange: [0, 1, 3, 5, 8],
+            outputRange: [1200, 600, 0, -600, -1200]
+        });
+
+        const xpos2 = this.animatedValue.interpolate({
+            inputRange: [0, 2, 4, 6, 8],
+            outputRange: [1200, 600, 0, -600, -1200]
+        });
+
+        const xpos3 = this.animatedValue.interpolate({
+            inputRange: [0, 3, 5, 7, 8],
+            outputRange: [1200, 600, 0, -600, -1200]
+        });
+
+        return(
+                <View style={{flex: 1, flexDirection: 'row', justifyContent: 'center'}}>
+                    <Animated.View style={{ width: '100%', transform: [{translateX: xpos1}]}}>
+                        <RenderItem item={this.props.dishes.dishes.filter((dish) => dish.featured)[0]}
+                            isLoading={this.props.dishes.isLoading}
+                            errMess={this.props.dishes.errMess} 
+                        />
+                    </Animated.View>
+                    <Animated.View style={{ width: '100%',  transform: [{translateX: xpos2}]}}>
+                        <RenderItem item={this.props.promotions.promotions.filter((promo) => promo.featured)[0]}
+                            isLoading={this.props.promotions.isLoading}
+                            errMess={this.props.promotions.errMess} 
+                        />
+                    </Animated.View>
+                    <Animated.View style={{ width: '100%',  transform: [{translateX: xpos3}]}}>
+                        <RenderItem item={this.props.leaders.leaders.filter((leader) => leader.featured)[0]}
+                            isLoading={this.props.leaders.isLoading}
+                            errMess={this.props.leaders.errMess} 
+                        />
+                    </Animated.View>
+                </View>
+            );
+        }
 }
 
 export default connect(mapStateToProps)(Home); //we need to connect this by saying connect(mapStateToProps)(Home);
