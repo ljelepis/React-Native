b1ab5aa1278282a77a7c012da9f067a00b42392c
Lindsey Jelepis <lin.jelepis@gmail.com>
2019-05-17 13:25:38 -0700

    Persist Redux Store

--- App.js
+++ App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import Main from './components/MainComponent';
 import { Provider } from 'react-redux';
 import { ConfigureStore } from './redux/configureStore';
+import { PersistGate } from 'redux-persist/es/integration/react'
+import { Loading } from './components/LoadingComponent';
+
+const { persistor, store } = ConfigureStore();
 
 const store = ConfigureStore();
 
@@ -9,7 +13,11 @@ export default class App extends React.Component {//created a component and then
   render() {
     return (//made our App.js file to connect to the redux store, via <Provider store={store}>
       <Provider store={store}>
-            <Main />
+        <PersistGate 
+          loading={<Loading />}
+          persistor={persistor}>
+          <Main />
+        </PersistGate>
       </Provider>
     );
   }
